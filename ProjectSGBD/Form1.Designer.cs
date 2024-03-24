fa28072f7b394743b0af1be13222036753671631
programator <danciulavu@gmail.com>
2024-03-24 10:25:38 +0200

    Lab 1

---
+++ ProjectSGBD/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ProjectSGBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(16, 15);
+            this.dataGridViewParent.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidth = 51;
+            this.dataGridViewParent.Size = new System.Drawing.Size(669, 185);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(708, 62);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(100, 28);
+            this.deleteBtn.TabIndex = 1;
+            this.deleteBtn.Text = "delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(741, 240);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(867, 62);
+            this.updateBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(100, 28);
+            this.updateBtn.TabIndex = 3;
+            this.updateBtn.Text = "update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(741, 188);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(16, 274);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowHeadersWidth = 51;
+            this.dataGridViewChild.RowTemplate.Height = 24;
+            this.dataGridViewChild.Size = new System.Drawing.Size(669, 217);
+            this.dataGridViewChild.TabIndex = 5;
+            this.dataGridViewChild.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewChild_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(867, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(867, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(867, 155);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(721, 99);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 9;
+            this.addBtn.Text = "add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button addBtn;
+    }
+}
+
---
+++ ProjectSGBD/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectSGBD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridViewChild();
+        }
+
+        private void InitializeDataGridViewChild()
+        {
+            dataGridViewChild.SelectionChanged += DataGridViewChild_SelectionChanged;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Selectati persoana!");
+                return;
+            }
+            string connectionString = "Data Source=DESKTOP-J9DDI9N;Initial Catalog=Halloween;Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand deleteCommand = new SqlCommand("DELETE FROM Person WHERE CNP=@cnp", conn);
+                    deleteCommand.Parameters.AddWithValue("@cnp", textBox1.Text);
+                    int deletedRowCount = deleteCommand.ExecuteNonQuery();
+                    MessageBox.Show("Numar de inregistrari sterse: " + deletedRowCount);
+                    conn.Close();
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Invalid params!");
+                return;
+            }
+            string connectionString = "Data Source=DESKTOP-J9DDI9N;Initial Catalog=Halloween;Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand updateCmd = new SqlCommand("UPDATE Person SET PersonName=@name, Age=@age WHERE CNP=@cnp", conn);
+                    updateCmd.Parameters.AddWithValue("@name", textBox2.Text);
+                    updateCmd.Parameters.AddWithValue("@age", textBox3.Text);
+                    updateCmd.Parameters.AddWithValue("@cnp", textBox1.Text);
+                    int updateRowCount = updateCmd.ExecuteNonQuery();
+                    MessageBox.Show("Numar inregistrari updatate: " + updateRowCount);
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Mesajul erorii: \n{0}", ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectingString = "Data Source=DESKTOP-J9DDI9N;Initial Catalog=Halloween;Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectingString))
+                {
+                    conn.Open();
+                    MessageBox.Show("Starea conexiunii: " + conn.State.ToString());
+                    DataSet dataSet = new DataSet();
+                    SqlDataAdapter parentAdapter = new SqlDataAdapter("SELECT * FROM Halloween_Candy", conn);
+                    SqlDataAdapter childAdapter = new SqlDataAdapter("SELECT * FROM Person", conn);
+                    parentAdapter.Fill(dataSet, "Halloween_Candy");
+                    childAdapter.Fill(dataSet, "Person");
+                    BindingSource parentBS = new BindingSource();
+                    BindingSource childBS = new BindingSource();
+                    parentBS.DataSource = dataSet.Tables["Halloween_Candy"];
+                    dataGridViewParent.DataSource = parentBS;
+                    DataColumn parentPK = dataSet.Tables["Halloween_Candy"].Columns["IdCandy"];
+                    DataColumn childFK = dataSet.Tables["Person"].Columns["FavoriteCandy"];
+                    DataRelation relation = new DataRelation("fk_parent_child", parentPK, childFK);
+                    dataSet.Relations.Add(relation);
+                    childBS.DataSource = parentBS;
+                    childBS.DataMember = "fk_parent_child";
+                    dataGridViewChild.DataSource = childBS;
+                    conn.Close();
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show (ex.Message.ToString());
+            }
+        }
+
+        private void DataGridViewChild_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedRows.Count > 0)
+            {
+                string connectingString = "Data Source=DESKTOP-J9DDI9N;Initial Catalog=Halloween;Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False";
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(connectingString))
+                    {
+                        int selectedIndex = dataGridViewChild.SelectedRows[0].Index;
+                        conn.Open();
+                        DataSet dataSet = new DataSet();
+                        SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Person", conn);
+                        adapter.Fill(dataSet, "Person");
+                        DataRow selectedRow = dataSet.Tables["Person"].Rows[selectedIndex];
+                        textBox1.Text = selectedRow["CNP"].ToString();
+                        textBox2.Text = selectedRow["PersonName"].ToString();
+                        textBox3.Text = selectedRow["Age"].ToString();
+                        conn.Close();
+                    }
+                } catch (System.Exception ex)
+                {
+                    MessageBox.Show (ex.Message.ToString());
+                }
+            } else
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+        }
+
+        private void dataGridViewChild_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Invalid params!");
+                return;
+            }
+            if (dataGridViewParent.SelectedRows.Count > 0)
+            {
+                string connectionString = "Data Source=DESKTOP-J9DDI9N;Initial Catalog=Halloween;Integrated Security=True;Connect " +
+                "Timeout=30;Encrypt=False";
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        int selectedIndex = dataGridViewParent.SelectedRows[0].Index;
+                        con.Open();
+                        DataSet data = new DataSet();
+                        SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Halloween_Candy", con);
+                        adapter.Fill(data, "Halloween_Candy");
+                        DataRow dataRow = data.Tables["Halloween_Candy"].Rows[selectedIndex];
+                        string candy = dataRow["IdCandy"].ToString();
+                        SqlCommand addCmd = new SqlCommand("INSERT INTO Person(CNP, PersonName, Age, FavoriteCandy) VALUES (@cnp, @name, @age, @candy)", con);
+                        addCmd.Parameters.AddWithValue("@cnp", textBox1.Text);
+                        addCmd.Parameters.AddWithValue("@name", textBox2.Text);
+                        addCmd.Parameters.AddWithValue("@age", textBox3.Text);
+                        addCmd.Parameters.AddWithValue("@candy", candy);
+                        int insertRowCount = addCmd.ExecuteNonQuery();
+                        MessageBox.Show("Inregistrari inserate: " + insertRowCount);
+                        con.Close();
+                    }
+                } catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Mesajul erorii: \n{0}", ex.Message);
+                }
+            } else
+            {
+                MessageBox.Show("Select candy!");
+            }
+        }
+    }
+}
